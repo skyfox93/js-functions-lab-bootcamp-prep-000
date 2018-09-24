34adda578c31ae2568f127ee26f9fe8f142eef93
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 15:14:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // write your code below
 function happyHolidays(){return "Happy Holidays!"}
-function  happyHolidayTo(){return "Happy ${holiday, ${name}!"}
-function HolidayCountdown(holiday,days){return "It's `${days} until ${holiday!}!`}
+function  happyHolidayTo(){return `Happy ${holiday}, ${name}!`}
+function HolidayCountdown(holiday,days){return `It is ${days} until ${holiday!}!`}
